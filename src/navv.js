186fd639f2ea843d764f2d70e07d0f1da883294c
vinicioscastro <viniciospcastro19@gmail.com>
2022-02-26 16:29:05 -0300

    Iniciando NavBar

---
+++ src/navv.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './navv.css'
+export default function Navv(){
+    return(
+        <div className="nav">
+            <h1>
+            Duque Bacelar Odondo
+        </h1>
+        </div>
+        
+    )
+}

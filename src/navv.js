07e9e29311742d35a42cec076cd967374d72ba24
vinicioscastro <viniciospcastro19@gmail.com>
2022-02-27 10:53:37 -0300

    fazendo navbar 01

--- src/navv.js
+++ src/navv.js
@@ -1,12 +1,23 @@
 import React from "react";
-import './navv.css'
-export default function Navv(){
-    return(
-        <div className="nav">
-            <h1>
-            Duque Bacelar Odondo
-        </h1>
-        </div>
-        
-    )
+import "./navv.css";
+export default function Navv() {
+  return (
+    <div className="nav">
+      <h1>Duque Bacelar Odondo</h1>
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Sobre</a>
+        </li>
+        <li>
+          <a href="#">Serviços</a>
+        </li>
+        <li>
+          <a href="#">Contatos</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
